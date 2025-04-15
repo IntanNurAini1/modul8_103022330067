eb6dc43dc22056139a76281866d49cff418761c2
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-15 08:13:57 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class BankTransferConfig
+{
+    public class Transfer
+    {
+        public string threshold { get; set; }
+        public string low_fee { get; set; }
+        public string high_fee { get; set; }
+    }
+    public class Confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+    }
+    public string lang { get; set; }
+
+    public Transfer transfer { get; set; }
+
+    public string[] methods { get; set; }
+
+    public Confirmation confirmation { get; set; }
+    public BankTransferConfig()
+    {
+    }
+    public BankTransferConfig(string lang, Transfer transfer, string[] methods, Confirmation confirmation)
+    {
+        this.lang = lang;
+        this.transfer = transfer;
+        this.methods = methods;
+        this.confirmation = confirmation;
+    }
+}
+
+class BankConfigUI
+{
+    public BankTransferConfig config { get; set; }
+    public const string configFile = "bank_transfer_config.json";
+    private BankTransferConfig ReadConfig()
+    {
+        string configData = File.ReadAllText(configFile);
+        config = JsonSerializer.Deserialize<BankTransferConfig>(configData);
+        return config;
+    }
+    private void WriteConfig()
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        string jsonString = JsonSerializer.Serialize(config, options);
+        File.WriteAllText(configFile, jsonString);
+    }
+
+    private void setDefaultConfig()
+    {
+        config = new BankTransferConfig
+        {
+            lang = "en",
+            transfer = new BankTransferConfig.Transfer
+            {
+                threshold = "25000000",
+                low_fee = "6500",
+                high_fee = "15000"
+            },
+            methods = new string[] { "RTO(real - time)", "SKN", "RTGS", "BI FAST" },
+            confirmation = new BankTransferConfig.Confirmation
+            {
+                en = "Yes",
+                id = "Ya"
+            }
+        };
+        WriteConfig();
+    }
+    public BankConfigUI()
+    {
+        try
+        {
+            config = ReadConfig();
+        }
+        catch (FileNotFoundException)
+        {
+            setDefaultConfig();
+            WriteConfig();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,118 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        BankConfigUI bankConfigUI = new BankConfigUI();
+        Console.WriteLine("Language en or id");
+        bankConfigUI.config.lang = Console.ReadLine();
+        if (bankConfigUI.config.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+            string amount = Console.ReadLine();
+            if (int.TryParse(amount, out int amountValue))
+            {
+                if (amountValue <= Convert.ToInt32(bankConfigUI.config.transfer.threshold))
+                {
+                    Console.WriteLine("Your transfer is below the maximum threshold.");
+                    int fee = Convert.ToInt32(bankConfigUI.config.transfer.low_fee);
+                    int hasil = amountValue + fee;
+                    Console.WriteLine("Total amount : " + hasil);
+                    Console.WriteLine("Select transfer method:");
+                    foreach (var method in bankConfigUI.config.methods)
+                    {
+                        Console.WriteLine(method);
+                    }
+                    string selectedMethod = Console.ReadLine();
+                    Console.WriteLine("Please type 'Yes' to confirm the transaction");
+                    string confirmation = Console.ReadLine();
+                    if (confirmation == "Yes")
+                    {
+                        Console.WriteLine("The transfer is completed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Transfer is cancelled.");
+                    }
+
+                }
+                else if (amountValue > Convert.ToInt32(bankConfigUI.config.transfer.threshold))
+                {
+                    Console.WriteLine("Your transfer is above the maximum threshold.");
+                    int fee = Convert.ToInt32(bankConfigUI.config.transfer.high_fee);
+                    int hasil = amountValue + fee;
+                    Console.WriteLine("Total amount : " + hasil);
+                    Console.WriteLine("Select transfer method:");
+                    foreach (var method in bankConfigUI.config.methods)
+                    {
+                        Console.WriteLine(method);
+                    }
+                    string selectedMethod = Console.ReadLine();
+                    Console.WriteLine("Please type 'Yes' to confirm the transaction");
+                    string confirmation = Console.ReadLine();
+                    if (confirmation == "Yes")
+                    {
+                        Console.WriteLine("The transfer is completed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Transfer is cancelled.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Masukan nominal yang ingin ditransfer :");
+                string amounts = Console.ReadLine();
+                if (int.TryParse(amounts, out int amountValues))
+                {
+                    if (amountValues <= Convert.ToInt32(bankConfigUI.config.transfer.threshold))
+                    {
+                        Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+                        int fee = Convert.ToInt32(bankConfigUI.config.transfer.low_fee);
+                        int hasil = amountValue + fee;
+                        Console.WriteLine("Total biaya : " + hasil);
+                        Console.WriteLine("Pilih metode transfer: ");
+                        foreach (var method in bankConfigUI.config.methods)
+                        {
+                            Console.WriteLine(method);
+                        }
+                        string selectedMethod = Console.ReadLine();
+                        Console.WriteLine(" Ketik 'Ya' untuk mengkonfirmasi transaksi");
+                        string confirmation = Console.ReadLine();
+                        if (confirmation == "Ya")
+                        {
+                            Console.WriteLine("Proses transfer berhasil");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Proses transfer dibatalkan");
+                        }
+
+                    }
+                    else if (amountValue > Convert.ToInt32(bankConfigUI.config.transfer.threshold))
+                    {
+                        Console.WriteLine("Your transfer is above the maximum threshold.");
+                        int fee = Convert.ToInt32(bankConfigUI.config.transfer.high_fee);
+                        int hasil = amountValue + fee;
+                        Console.WriteLine("Total amount : " + hasil);
+                        Console.WriteLine("Select transfer method:");
+                        foreach (var method in bankConfigUI.config.methods)
+                        {
+                            Console.WriteLine(method);
+                        }
+                        string selectedMethod = Console.ReadLine();
+                        Console.WriteLine(" Ketik 'Ya' untuk mengkonfirmasi transaksi");
+                        string confirmation = Console.ReadLine();
+                        if (confirmation == "Yes")
+                        {
+                            Console.WriteLine("Proses transfer berhasil");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Proses transfer dibatalkan");
+                        }
+                    }
+                }
+        }
+    }
+}
